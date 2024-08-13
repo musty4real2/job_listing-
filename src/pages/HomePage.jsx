5dd298c21c7013d0444b1146e36077747e90cdd5
musty4real2 <musty4real2@gmail.com>
2024-08-13 23:34:24 +0100

    addition

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
         <>
         <Hero title="Become a React Dev" subtitle="Find the React job that fits your skills and needs" />
         <HomeCards />
-        <JobListings />
+        <JobListings isHome={true} />
         <ViewAllJobs />
         </>
     )
